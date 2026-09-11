cc75cf5bc27457065639ea5644fa52291aaa78a7
Thomas Vu <thomasjvu@gmail.com>
2026-09-11 17:59:48 -0400

    Sync @phantasy/plugin-messenger (source-extracted) from monorepo export

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export declare class MessengerPlugin {
+  name: string;
+  version: string;
+  description: string;
+  getTools(): never[];
+}
+export default MessengerPlugin;
---
+++ dist/index.js
@@ -0,0 +1,9 @@
+export class MessengerPlugin {
+  name = "messenger";
+  version = "0.1.17-beta";
+  description = "Facebook Messenger integration plugin for Phantasy companions.";
+  getTools() {
+    return [];
+  }
+}
+export default MessengerPlugin;
